970f85a11fed720d6e28bdcaa926bd361aa932a0
Kymani Emmanuel Glimeti <kymani20100@gmail.com>
2021-04-27 14:02:01 +0000

    Modifications

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { useDispatch, useSelector} from "react-redux";
 import {useHistory} from "react-router-dom";
 import { auth, provider } from "../firebase";
-import { selectUsername, selectUserPhoto, selectUserLoginDetails, setUserLoginDetails, setSignOutState} from "../features/user/userSlice";
+import { selectUsername, selectUserPhoto, setUserLoginDetails, setSignOutState} from "../features/user/userSlice";
 
 const Header = (props) => {
   const dispatch = useDispatch();
